b28a579272a79e77776434f2afd579a69ad98eb6
unknown <oscpe436@PC2-1.helix.ida.liu.se>
2015-09-23 13:06:34 +0200

    vignnete added

--- R/functions.R
+++ R/functions.R
@@ -19,15 +19,14 @@
 # content(a)
 
 
-
-GetKolada <- function(kpi_type, municipalities, year=""){
+GetKolada <- function(kpi_id, municipalities, year=""){
   
   #   browser()
-  stopifnot(is.character(kpi_type), is.character(municipalities))
-  stopifnot(sum(sapply(municipalities, FUN = function(x) !x %in% munic_code[,1]))==0)    
+  stopifnot(is.character(kpi_id), is.character(municipalities))
+#   stopifnot(sum(sapply(municipalities, FUN = function(x) !x %in% munic_code[,1]))==0)    
   
   munic2 <- paste(municipalities, collapse=",")  
-  kpi_t2 <- paste(kpi_type, collapse=",")
+  kpi_t2 <- paste(kpi_id, collapse=",")
   
   if(identical(year, "")) year2 <- "" else {
     stopifnot(sum(is.na(as.numeric(year))) == 0)
@@ -35,12 +34,12 @@ GetKolada <- function(kpi_type, municipalities, year=""){
   }  
   
   url <- paste("http://api.kolada.se/v2/data/kpi/", kpi_t2, "/municipality/", munic2, year2, sep="")
-  response <- GET(url)
+  response <- httr::GET(url)
   
-  stopifnot(status_code(response)>=200 & status_code(response)<300)
-  stopifnot(content(response)$count>0)
+  stopifnot(httr::status_code(response)>=200 & httr::status_code(response)<300)
+  stopifnot(httr::content(response)$count>0)
   
-  Kolada_list <- content(response)$values
+  Kolada_list <- httr::content(response)$values
   n <- length(Kolada_list)
   
   kpi <- character(n)
@@ -68,7 +67,7 @@ GetKolada <- function(kpi_type, municipalities, year=""){
 #     print(i)
   }
   
-  Kolada_df <- data.frame(kpi, municipality=munip, year, female_value, male_value, tot_value)
+  Kolada_df <- data.frame(kpi, municipality=munip, year, female=female_value, male=male_value, total=tot_value)
   
   return(Kolada_df)
   
@@ -82,7 +81,7 @@ a <- GetKolada("N00041", "0580")
 a <- GetKolada(c("N00090", "N00041"), c("0580", "0180"))
 a <- GetKolada("N00090", "0580", 2013)
 a <- GetKolada("N00090", "0580", 2010:2014)
-a <- GetKolada(c("N00090", "N00041"), c("0580", "0180"), "2013")
+a <- GetKolada(c("N00090", "N00041"), c("0580", "0180"), 2010:2014)
 a <- GetKolada("N00105", "0580")
 
 
